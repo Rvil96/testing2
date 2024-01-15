b60076fae628cb20baf6e066eba2fc4a267aa005
Rvil96 <156665137+Rvil96@users.noreply.github.com>
2024-01-15 16:12:44 +0400

    ТесткКомит

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         System.out.println("Введите выражение формата а+b, где значение а и b должно находится в диапозоне от 1 до 10.\n" +
                 "В формате арабских цифр или римских. Для завершение команды введите Exit.");
-        Culcl x = new Culcl();
+        Culcl x = new Culcl();// объявляем объект класса
         Scanner scr = new Scanner(System.in);
         String input = "input";
 
